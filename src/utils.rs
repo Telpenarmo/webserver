911e25932ae1b31adf7a280d8d3c04bf449b06d0
Telpenarmo <telpenarmo@protonmail.com>
2023-02-19 19:09:44 +0100

    fix setting charset for text files

--- src/utils.rs
+++ src/utils.rs
@@ -4,8 +4,8 @@ use std::path::Path;
 
 pub fn match_file_type(filename: &Path) -> &str {
     match filename.extension().and_then(OsStr::to_str) {
-        Some("txt") => "text/plain, charset=utf-8",
-        Some("html") => "text/html, charset=utf-8",
+        Some("txt") => "text/plain; charset=utf-8",
+        Some("html") => "text/html; charset=utf-8",
         Some("css") => "text/css",
         Some("js") => "application/javascript",
         Some("jpg") => "image/jpeg",
