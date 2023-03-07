f5707e08ee87f13e6622c7fdf253036008a41de0
Telpenarmo <telpenarmo@protonmail.com>
2023-03-07 19:35:50 +0100

    use crate for guessing mime types

--- src/utils.rs
+++ src/utils.rs
@@ -1,17 +1,15 @@
-use std::ffi::OsStr;
 use std::path::Path;
 
-pub fn match_file_type(filename: &Path) -> &str {
-    match filename.extension().and_then(OsStr::to_str) {
-        Some("txt") => "text/plain; charset=utf-8",
-        Some("html") => "text/html; charset=utf-8",
-        Some("css") => "text/css",
-        Some("js") => "application/javascript",
-        Some("jpg") => "image/jpeg",
-        Some("jpeg") => "image/jpeg",
-        Some("png") => "image/png",
-        Some("pdf") => "application/pdf",
-        Some("json") => "application/json",
-        _ => "application/octet-stream",
-    }
+pub fn match_file_type(filename: &Path) -> String {
+    let guess = mime_guess::from_path(filename);
+    let mime = match guess.first() {
+        None => mime_guess::mime::APPLICATION_OCTET_STREAM,
+        Some(mime) => mime,
+    };
+    let mime = if mime == mime_guess::mime::TEXT_PLAIN {
+        mime_guess::mime::TEXT_PLAIN_UTF_8
+    } else {
+        mime
+    };
+    mime.to_string()
 }
